d059d54d9bbfed4d915f1e550d3b1db06dbb4676
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-01-19 10:54:23 +0100

    adding first contract

---
+++ contract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PotatoToken is ERC20 {
+    // The cost of one potato in tokens
+    uint256 public constant POTATO_PRICE = 1000;
+
+    // Mapping of address to number of potatoes bought
+    mapping(address => uint256) private potatoesBought;
+
+    constructor() ERC20("PotatoToken", "PTT") {
+    }
+
+    // Function to mint tokens to a user address
+    // TODO Change the function so you can mint only once every 5 blocks.
+    function mint() public {
+        uint256 amount = 10;
+        _mint(msg.sender, amount);
+    }
+
+    // Function to buy potatoes using tokens
+    function buyPotato() public {
+        // Check if the user has enough tokens to buy a potato
+        require(balanceOf(msg.sender) >= POTATO_PRICE, "Not enough tokens to buy a potato");
+
+        // Burn the tokens used to buy the potato
+        _burn(msg.sender, POTATO_PRICE);
+
+        // Increment the potato counter for the user
+        potatoesBought[msg.sender]++;
+    }
+
+    // Function to get the number of potatoes bought by an address
+    function getPotatoesBought(address user) public view returns (uint256) {
+        return potatoesBought[user];
+    }
+}
